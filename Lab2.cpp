9a64b2e71f1434afbd2df85b2ac9c04f1abe1201
DragonKeker <47860210+DragonKeker@users.noreply.github.com>
2019-12-23 13:19:25 +0300

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -4,6 +4,7 @@
 #include "logika.h"
 
 int main() {
+	std::cout << 1_l << std::endl;
 	setlocale(LC_ALL, "RUSSIAN");
 	int k;
 	logika obj1;
@@ -12,25 +13,23 @@ int main() {
 	"Введите данные"_out;
 	std::cin >> obj1;
 	std::cin >> obj2;
-	std::cout << (obj1&obj2);
-	std::cout << (obj1|obj2);
-	std::cout << (obj1^obj2);
+	std::cout << (obj1 & obj2);
+	std::cout << (obj1 | obj2);
+	std::cout << (obj1 ^ obj2);
 	std::cout << (~obj1);
 	"Введите : k "_out;
 	std::cin >> k;
-	std::cout << (obj1<<k);
+	std::cout << (obj1 << k);
 	"Введите : k "_out;
 	std::cin >> k;
-	std::cout << (obj1>>k);
+	std::cout << (obj1 >> k);
 	"Всего едениц в первой строке:"_out;
-	std::cout  << (obj1._count1()) << "\n";
+	std::cout << (obj1._count1()) << "\n";
 	obj1._sravnenie(obj2);
 	if (obj1._vkluch(obj2))
-		 "Включает"_out;
+		"Включает"_out;
 	else
 		"Не включает"_out;
-
-	obj3.field1 = 101010110_l;
-	obj3.field2 = 101010111_l;
-	std::cout << obj3 << std::endl;
+	
+	std::cout << 100000000000000000000000000000000000000000000000000000000000000000_l << std::endl;
 }
